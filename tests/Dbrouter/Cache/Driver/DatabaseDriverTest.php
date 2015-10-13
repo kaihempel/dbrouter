9fea1308b0d8ca164094e9b04d27de0125b70de8
Kai Hempel <kai.hempel@kuweh.de>
2015-10-13 08:31:36 +0200

    Cache driver test

--- tests/Dbrouter/Cache/Driver/DatabaseDriverTest.php
+++ tests/Dbrouter/Cache/Driver/DatabaseDriverTest.php
@@ -16,8 +16,59 @@ use Mockery as m;
  */
 class UrlProviderTest extends PHPUnit_Framework_TestCase
 {
+    public function setUp()
+    {
+        $db = m::mock('Doctrine\DBAL\Connection');
+        $db->shouldReceive('setFetchMode');
+        $db->shouldReceive('setNestTransactionsWithSavepoints');
+        $db->shouldReceive('beginTransaction');
+        $db->shouldReceive('commit');
+        $db->shouldReceive('quote');
+        $db->shouldReceive('fetchColumn')->andReturn(array('table' => 'test'));
+
+        $factory = m::mock('alias:Dbrouter\Database\ConnectionFactory');
+        $factory->shouldReceive('make')->andReturn($db);
+    }
+
     public function testNewDriver()
     {
-        $driver = new DatabaseDriver();
+        $driver = new DatabaseDriver(array('test'));
+    }
+
+    public function testSetTable()
+    {
+        $driver = new DatabaseDriver(array('test'), 'test');
+    }
+
+    // Exception tests
+
+    /**
+     * @expectedException Dbrouter\Exception\Cache\DriverException
+     */
+    public function testNewDriverNoConnection()
+    {
+        m::resetContainer();
+
+        $factory = m::mock('alias:Dbrouter\Database\ConnectionFactory');
+        $factory->shouldReceive('make')->andThrow(\Dbrouter\Exception\Database\ConnectionFactoryException::make('No connection parameter given!'));
+
+        $driver = new DatabaseDriver(array('test'));
+    }
+
+    /**
+     * @expectedException Dbrouter\Exception\Cache\DriverException
+     */
+    public function testSetNonExistingTable()
+    {
+        m::resetContainer();
+
+        $db = m::mock('Doctrine\DBAL\Connection');
+        $db->shouldReceive('fetchColumn')->andReturnNull();
+
+        $factory = m::mock('alias:Dbrouter\Database\ConnectionFactory');
+        $factory->shouldReceive('make')->andReturn($db);
+
+        $driver = new DatabaseDriver(array('test'));
+        $driver->setTable('test');
     }
 }
